95c359322620bb0b69560bc11cc6dcb5989411db
idrees khan <khanidrees1234@gmail.com>
2024-09-04 07:49:57 +0000

    completed start component

---
+++ src/components/StarComponent.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+const StarComponent = ({ starCount = 5 }) => {
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
+  return (
+    <div style={{ cursor: "pointer" }} className="container">
+      {new Array(starCount).fill(0).map((_, index) => {
+        return (
+          <span
+            key={index.toString()}
+            className={
+              (hover == 0 && index < rating) || index < hover ? "golden" : ""
+            }
+            onClick={(e) => {
+              setRating(index + 1);
+            }}
+            onMouseEnter={(e) => {
+              setHover(index + 1);
+            }}
+            onMouseLeave={(e) => {
+              setHover(0);
+            }}
+          >
+            &#9733;
+          </span>
+        );
+      })}
+    </div>
+  );
+};
+
+export default StarComponent;
